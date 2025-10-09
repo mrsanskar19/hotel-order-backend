d1c04278fd793b1b7e889a6920b9d4b681a043b8
mrsanskar19 <mr.sanskar19@gmail.com>
2025-10-09 19:04:30 +0530

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrderGateway } from './ws/order.gateway';
 import { PrismaService } from './prisma/prisma.service';
-import { HotelsModule } from './hotels/hotels.module';
-import { MenuModule } from './menu/menu.module';
-import { OrdersModule } from './orders/orders.module';
-import { ReviewModule } from './review/review.module';
-import { AuthModule } from './auth/auth.module';
-import { MenuItemModule } from './menu-item/menu-item.module';
 import { OrderModule } from './order/orders.module';
 
 @Module({
   imports: [
-    HotelsModule,
-    MenuModule,
-    OrdersModule,
-    ReviewModule,
-    AuthModule,
-    MenuItemModule,
     OrderModule
   ],
   controllers: [AppController],
